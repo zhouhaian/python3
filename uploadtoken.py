f7264d19d99ab0e02389dbf3ebdccb0160c08e3a
zhouhaian <41657808+zhouhaian@users.noreply.github.com>
2021-10-11 15:43:17 +0800

    Update uploadtoken.py

--- uploadtoken.py
+++ uploadtoken.py
@@ -4,12 +4,7 @@ import json
 import time
 
 
-# 七牛账号AK/SK
-AccessKey = ''
-SecretKey = ''
-
-
-def UploadToken(bucket, key = None, deadline = None, policy = None):
+def UploadToken(accessKey, secretKey, bucket, key=None, deadline=None, policy=None):
     # 1）构建上传策略
     if key is None:
         scope = bucket
@@ -19,35 +14,43 @@ def UploadToken(bucket, key = None, deadline = None, policy = None):
     if deadline is None:
         deadline = int(time.time()) + 3600
 
-    PutPolicyDict = {"scope": scope, "deadline": deadline}
+    putPolicyDict = {"scope": scope, "deadline": deadline}
 
     if policy is not None:
         # dict()将2个dict数据合并，形成完整的上传策略
-        PutPolicyDict = dict(PutPolicyDict, **policy)
+        putPolicyDict = dict(putPolicyDict, **policy)
 
     # json.dumps()，将dict型数据转成string型
-    PutPolicy = json.dumps(PutPolicyDict, separators=(',', ':'))
+    fPutPolicy = json.dumps(putPolicyDict, separators=(',', ':'))
 
     # base64.urlsafe_b64encode()需要传入值为byte型
     # 得到base64url安全编码后的上传策略
-    encodedPutPolicy = base64.urlsafe_b64encode(PutPolicy.encode('utf-8'))
+    encodedPutPolicy = base64.urlsafe_b64encode(fPutPolicy.encode('utf-8'))
 
     # 2）计算签名-Signature
     # 签名使用HMAC-SHA1算法
     # 签名结果要求进行base64url安全编码
-    Signature = hmac.new(SecretKey.encode('utf-8'), encodedPutPolicy, digestmod='sha1').digest()
-    encodedSignature = base64.urlsafe_b64encode(Signature)
+    signature = hmac.new(secretKey.encode('utf-8'), encodedPutPolicy, digestmod='sha1').digest()
+    encodedSignature = base64.urlsafe_b64encode(signature)
 
     # 3）构建上传凭证-UploadToken
-    # UploadToken = AK:Signature:PutPolicy
-    UploadToken = AccessKey + ':' + encodedSignature.decode('utf-8') + ':' + encodedPutPolicy.decode('utf-8')
+    # uploadToken = AK:Signature:PutPolicy
+    uploadToken = accessKey + ':' + encodedSignature.decode('utf-8') + ':' + encodedPutPolicy.decode('utf-8')
 
-    return UploadToken
+    return uploadToken
 
 
-bucket = 'test'
-key = 'test.jpg'
-policy = {"isPrefixalScope": 1}
-deadline = 1627965292
+if __name__ == '__main__':
+    # 七牛账号AK/SK
+    accessKey = ''
+    secretKey = ''
+    bucket = 'test'
+    key = 'test.jpg'
+    policy = {
+        "isPrefixalScope": 1,
+        "mimeLimit": "image/jpeg"
+    }
+    # deadline为时间戳格式
+    deadline = 1627965292
 
-print(UploadToken(bucket=bucket, key=key, deadline=deadline, policy=policy))
+    print(UploadToken(accessKey, secretKey, bucket=bucket, key=key, deadline=deadline, policy=policy))
