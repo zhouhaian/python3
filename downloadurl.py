8af811ff0c9a9ab89c13008851419a9c88fdc1ad
zhouhaian <41657808+zhouhaian@users.noreply.github.com>
2021-10-11 15:37:04 +0800

    Update downloadurl.py

--- downloadurl.py
+++ downloadurl.py
@@ -3,30 +3,27 @@ import hmac
 import time
 
 
-# 七牛账号AK/SK
-AccessKey = ''
-SecretKey = ''
-
-
 # 生成私有空间文件下载url，url默认有效时间3600s
-def DownloadURL(DownloadUrl, expire=3600):
+def DownloadURL(accessKey, secretKey, downloadUrl, expire=3600):
     deadline = int(time.time()) + expire
-    DownloadUrl = DownloadUrl + '?e=' + str(deadline)
+    downloadUrl = downloadUrl + '?e=' + str(deadline)
 
     # 签算结果需要base64url安全编码
     # hmac.new(key,data,算法)，key和data要求传入值为byte型
-    Signature = hmac.new(SecretKey.encode('utf-8'), DownloadUrl.encode('utf-8'), digestmod='sha1')
+    Signature = hmac.new(secretKey.encode('utf-8'), downloadUrl.encode('utf-8'), digestmod='sha1')
     # base64.urlsafe_b64encode(data)，data要求传入值为byte型
     encodedSignature = base64.urlsafe_b64encode(Signature.digest())
     # print(encodedSignature)
-    # 构建下载凭证AK:Signature
-    Token = AccessKey + ':' + encodedSignature.decode('utf-8')
+    # DowanloadToken = AK:Signature
+    Token = accessKey + ':' + encodedSignature.decode('utf-8')
 
     # 构建下载url
-    PDownloadURL = DownloadUrl + '&' + 'token=' + Token
+    pDownloadURL = downloadUrl + '&' + 'token=' + Token
 
-    return PDownloadURL
+    return pDownloadURL
 
 
-DownloadUrl = 'http://kodo.qiniu.com/test.jpg'
-print(DownloadURL(DownloadUrl, expire=3600))
+accessKey = ''
+secretKey = ''
+downloadUrl = 'http://kodo.qiniu.com/test.jpg'
+print(DownloadURL(accessKey, secretKey, downloadUrl))
