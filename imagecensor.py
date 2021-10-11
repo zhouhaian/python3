63f3542412bc45727d34044cf67ae949ed97ddeb
zhouhaian <41657808+zhouhaian@users.noreply.github.com>
2021-10-11 15:52:23 +0800

    Update imagecensor.py

--- imagecensor.py
+++ imagecensor.py
@@ -1,17 +1,18 @@
 import requests
+import json
 from accesstoken import AccessToken
 
 
-def imagecensor(accesskey, secretkey, body):
+def ImageCensor(accessKey, secretKey, body):
     method = "POST"
     path = "/v3/image/censor"
     host = "ai.qiniuapi.com"
     contentType = "application/json"
 
     # 签算管理token以及将body从dict转成json格式
-    accesstoken, body = AccessToken(accesskey, secretkey, method, path, host, contenttype=contentType, body=body)
+    accesstoken, body = AccessToken(accessKey, secretKey, method, path, host, contentType=contentType, body=body)
     # print(accesstoken)
-    
+
     url = "http://" + host + path
     header = {
         'Host': host,
@@ -26,12 +27,12 @@ def imagecensor(accesskey, secretkey, body):
 
 if __name__ == '__main__':
     # 七牛账号AK/SK
-    AccessKey = ''
-    SecretKey = ''
+    accessKey = ''
+    secretKey = ''
     body = {
         "data": {
             # 图片URL地址，必传参数，目前支持http和https
-            "uri": ''
+            "uri": 'http://test.qiniu.com/test.png'
         },
         "params": {
             # 审核类型，必传参数，没有默认值，可选项：pulp/terror/politician。
@@ -39,4 +40,4 @@ if __name__ == '__main__':
         }
     }
 
-    print(imagecensor(AccessKey, SecretKey, body))
+    print(ImageCensor(accessKey, secretKey, body))
