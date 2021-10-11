3ddda97a6014d3943e13cb0d4cef841a246d7d05
zhouhaian <41657808+zhouhaian@users.noreply.github.com>
2021-10-11 16:13:31 +0800

    Update listv2.py

--- listv2.py
+++ listv2.py
@@ -3,34 +3,33 @@ from accesstoken import AccessToken
 
 
 # ak、sk、bucket必需参数，limit范围1-1000
-def listv2(accesskey, secretkey, bucket, limit=1000, prefix=None, marker=None, delimiter=None):
+def Listv2(accessKey, secretKey, bucket, limit=1000, prefix=None, marker=None, delimiter=None):
     method = 'GET'
     path = "/v2/list?bucket=" + bucket + "&limit=" + str(limit)
     host = "rsf.qbox.me"
     contentType = "application/x-www-form-urlencoded"
 
-    accesstoken, body = AccessToken(accesskey, secretkey, method, path, host, contenttype=contentType)
-    
+    accessToken, body = AccessToken(accessKey, secretKey, method, path, host, contentType=contentType)
+    # print("accessToken:", accessToken)
     url = "http://" + host + path
     header = {
         'Host': host,
-        'Authorization': accesstoken,
+        'Authorization': accessToken,
         'Content-Type': contentType
     }
 
     # headers传入值数据类型要求为dict
     res = requests.get(url, headers=header)
-    # print("accesstoken:", accesstoken)
     # print("url:", url)
     return res.content
 
 
 if __name__ == '__main__':
-    access_key = ''
-    secret_key = ''
-    bucket_name = ''
+    accessKey = ''
+    secretKey = ''
+    bucket = 'theozhou'
     limit = 1
     prefix = None
     marker = None
     delimiter = None
-    print(listv2(access_key, secret_key, bucket_name, limit=limit))
+    print(Listv2(accessKey, secretKey, bucket, limit=limit).decode('utf-8'))
